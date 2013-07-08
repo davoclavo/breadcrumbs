0a4d88825712942f071bb643b512e2dd5e35f97f
Victor Borja <vborja@apache.org>
2013-07-07 23:56:18 -0500

    "I believe that Ronald Reagan will someday make this  country what it once was... an arctic wilderness." -- Steve Martin

--- js/main.js
+++ js/main.js
@@ -1,11 +1,36 @@
 var breadcrumbs = new Firebase('https://breadcrumbs.firebaseIO.com/');
+var trail, crumbs, users;
 
-//---------- TESTING
-var trailName = 'test';
+var pinShadow = new google.maps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_shadow",
+        new google.maps.Size(40, 37),
+        new google.maps.Point(0, 0),
+        new google.maps.Point(12, 35));
 
-trail = breadcrumbs.child('trails').child(trailName);
-crumbs = trail.child('crumbs');
-users = trail.child('users');
+var router = new (Backbone.Router.extend({
+  routes: {
+    'trail/:id': 'trail',
+    '':          'root'
+  },
+
+  root: function() {
+    router.navigate("trail/" + randomHex(6), {trigger: true, replace: true});
+  },
+
+  trail: function(id) {
+    trail = breadcrumbs.child('trails').child(id);
+    crumbs = trail.child('crumbs');
+    users = trail.child('users');
+
+    $("input[name=trail]").val(id);
+
+    loadTrail();
+  }
+}))
+
+console.log("router: ", router)
+
+Backbone.history.start({pushState: false});
+
 
 // crumbs.push({
 //   lat: -12.043333,
@@ -24,84 +49,90 @@ $(document).ready(function(){
       })
     }
   });
+
+  $("input[name=trail]").on('blur', function(e){
+     router.navigate("trail/" + this.value)
+  })
+
 });
 
+function loadTrail() {
 
-var localUsers = {};
+  var localUsers = {};
 
-users.on('value', function(s){
-  s.forEach(function(b) {
-    newUser(b);
-  });
-});
-
-users.on('child_changed', function(s){
-  var userMarker = localUsers[s.name()];
-  if(typeof userMarker === 'undefined') {
-    newUser(s);
-  }
-  else {
-    console.log(s.val().lat, s.val().lng);
-    userMarker.animatedMoveTo(s.val().lat, s.val().lng);
-  }
-});
-
-users.on('child_removed', function(s) {
-  var userMarker = localUsers[s.name()];
-  if(typeof userMarker !== 'undefined') {
-    userMarker.setMap(null);
-    delete localUsers[s.name()];
-  }
-});
-
-function newUser(u) {
-    // var marker = new google.maps.Marker({ icon: 'http://chart.googleapis.com/chart?chst=d_bubble_icon_text_small&chld=' + iconType + '|bbT|'+bus.routeTag+'|' + directionColor + '|eee', position: busLatLng, map: map });
-    var user = $.extend(u.val(), {'name': u.name()});
-    var color = user.isOwner ? 'FF0000' : '0000FF';
-    marker = map.addMarker({
-      lat: user.lat,
-      lng: user.lng,
-      icon: colorIcon(color),
-      shadow: pinShadow
+  users.on('value', function(s){
+    s.forEach(function(b) {
+      newUser(b);
     });
-    localUsers[user.name] = marker;
-}
-
-// crumbs.on('value', function(s){
-//   s.forEach(function(b) {
-//     newCrumb(b);
-//   });
-// })
-
-var localCrumbs = {};
-
-crumbs.on('child_added', function(s){
-  newCrumb(s);
-});
-
-crumbs.on("child_removed", function(s) {
-  var crumbMarker = localCrumbs[s.name()];
-  if(typeof crumbMarker !== 'undefined') {
-    crumbMarker.setMap(null);
-    delete localCrumbs[s.name()];
-  }
-});
-
-function newCrumb(c) {
-  // var marker = new google.maps.Marker({ icon: 'http://chart.googleapis.com/chart?chst=d_bubble_icon_text_small&chld=' + iconType + '|bbT|'+bus.routeTag+'|' + directionColor + '|eee', position: busLatLng, map: map });
-  var crumb = $.extend(c.val(), {'name': c.name()});
-  marker = map.addMarker({
-    lat: crumb.lat,
-    lng: crumb.lng,
-    icon: "/img/breadcrumb.png",
-    verticalAlign: 'center',
-    horizontalAlign: 'center'
   });
-  localCrumbs[crumb.name] = marker;
-  console.log('Crumb added!\n',crumb.lat,crumb.lng);
+
+  users.on('child_changed', function(s){
+    var userMarker = localUsers[s.name()];
+    if(typeof userMarker === 'undefined') {
+      newUser(s);
+    }
+    else {
+      console.log(s.val().lat, s.val().lng);
+      userMarker.animatedMoveTo(s.val().lat, s.val().lng);
+    }
+  });
+
+  users.on('child_removed', function(s) {
+    var userMarker = localUsers[s.name()];
+    if(typeof userMarker !== 'undefined') {
+      userMarker.setMap(null);
+      delete localUsers[s.name()];
+    }
+  });
+
+  function newUser(u) {
+      // var marker = new google.maps.Marker({ icon: 'http://chart.googleapis.com/chart?chst=d_bubble_icon_text_small&chld=' + iconType + '|bbT|'+bus.routeTag+'|' + directionColor + '|eee', position: busLatLng, map: map });
+      var user = $.extend(u.val(), {'name': u.name()});
+      var color = user.isOwner ? 'FF0000' : '0000FF';
+      marker = map.addMarker({
+        lat: user.lat,
+        lng: user.lng,
+        icon: colorIcon(color),
+        shadow: pinShadow
+      });
+      localUsers[user.name] = marker;
+  }
+
+  // crumbs.on('value', function(s){
+  //   s.forEach(function(b) {
+  //     newCrumb(b);
+  //   });
+  // })
+
+  var localCrumbs = {};
+
+  crumbs.on('child_added', function(s){
+    newCrumb(s);
+  });
+
+  crumbs.on("child_removed", function(s) {
+    var crumbMarker = localCrumbs[s.name()];
+    if(typeof crumbMarker !== 'undefined') {
+      crumbMarker.setMap(null);
+      delete localCrumbs[s.name()];
+    }
+  });
+
+  function newCrumb(c) {
+    // var marker = new google.maps.Marker({ icon: 'http://chart.googleapis.com/chart?chst=d_bubble_icon_text_small&chld=' + iconType + '|bbT|'+bus.routeTag+'|' + directionColor + '|eee', position: busLatLng, map: map });
+    var crumb = $.extend(c.val(), {'name': c.name()});
+    marker = map.addMarker({
+      lat: crumb.lat,
+      lng: crumb.lng,
+      icon: "/img/breadcrumb.png",
+      verticalAlign: 'center',
+      horizontalAlign: 'center'
+    });
+    localCrumbs[crumb.name] = marker;
+    console.log('Crumb added!\n',crumb.lat,crumb.lng);
+  }
+
 }
-
-
 
 
 // http://stackoverflow.com/a/7686977/756000
@@ -114,7 +145,8 @@ function colorIcon(color){
   return pinImage;
 }
 
-var pinShadow = new google.maps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_shadow",
-        new google.maps.Size(40, 37),
-        new google.maps.Point(0, 0),
-        new google.maps.Point(12, 35));
+function randomHex(len) {
+  return (new Array(len).join(0)
+          + parseInt(Math.pow(2, len * 4) * Math.random()
+                    ).toString(16)).slice(-len);
+}
