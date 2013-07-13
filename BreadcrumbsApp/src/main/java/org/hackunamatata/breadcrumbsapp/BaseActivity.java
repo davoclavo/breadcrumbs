e1712835e8fcbfef6a65af2d24f42747b67d8371
Victor Borja <vborja@apache.org>
2013-07-13 09:27:32 -0500

    Add firebase root getter

--- BreadcrumbsApp/src/main/java/org/hackunamatata/breadcrumbsapp/BaseActivity.java
+++ BreadcrumbsApp/src/main/java/org/hackunamatata/breadcrumbsapp/BaseActivity.java
@@ -2,11 +2,17 @@ package org.hackunamatata.breadcrumbsapp;
 
 import android.app.Activity;
 
+import com.firebase.client.Firebase;
 
 /**
  * Created by vic on 7/13/13.
  */
 public class BaseActivity extends Activity {
 
+    private static final String FIREBASE_URL = "https://breadcr";
+
+    public Firebase getFirebase() {
+        return new Firebase(FIREBASE_URL);
+    }
 
 }
