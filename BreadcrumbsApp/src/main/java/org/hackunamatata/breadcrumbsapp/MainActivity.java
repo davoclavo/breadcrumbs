8d56e2d75adae4801ca19d81658ffd20c99d75e4
Victor Borja <vborja@apache.org>
2013-07-23 20:44:12 -0500

    One good reason why computers can do more work than people is that they never have to stop and answer the phone.

--- BreadcrumbsApp/src/main/java/org/hackunamatata/breadcrumbsapp/MainActivity.java
+++ BreadcrumbsApp/src/main/java/org/hackunamatata/breadcrumbsapp/MainActivity.java
@@ -1,14 +1,27 @@
 package org.hackunamatata.breadcrumbsapp;
 
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 
-public class MainActivity extends BaseActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+
+public class MainActivity extends FragmentActivity {
+
+    private GoogleMap map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        map = ((SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
+        map.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        map.setMyLocationEnabled(true);
+        map.getUiSettings().setZoomControlsEnabled(false);
+        map.getUiSettings().setCompassEnabled(true);
+        map.getUiSettings().setMyLocationButtonEnabled(true);
     }
 
 
